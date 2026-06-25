360fd9cf11d9ca68f9f1ae33fe6e370ec9d94a98
abdrimovotabek990-stack <abdrimovotabek990@gmail.com>
2026-06-25 14:42:27 +0500

    status +

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/status', (req, res) => {
+  res.json({ status: 'Server is running' });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
